0839af13f7201a0071ac7bd4bc615ab1cf65ada8
tovrey <tovrey@gmail.com>
2018-02-11 19:17:20 -0500

    full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_titile}"
+    assert_select "title", @base_titile
   end
 
   test "should get help" do
